61908f987257f4e8e49fb9a58e831a11c189f212
Vladislav Degtyarenko <vladislavdegtyarenko@gmail.com>
2021-08-15 16:49:28 +0300

    adding deadline

--- app.js
+++ app.js
@@ -1,104 +1,129 @@
-const createTaskSection = document.querySelector(".create");
-const inputField = createTaskSection.querySelector(".create__input");
-const addTaskButton = createTaskSection.querySelector(".create__button");
-const taskList = document.querySelector(".list");
+const createTaskSection = document.querySelector('.newtask');
+const inputNameField = createTaskSection.querySelector('.newtask__name');
+const addTaskButton = createTaskSection.querySelector('.newtask__create');
+const setDateBtn = createTaskSection.querySelector('#setDateBtn');
+const setTimeBtn = createTaskSection.querySelector('#setTimeBtn');
+const taskListInDOM = document.querySelector('.list');
 
 const createNewTask = () => {
-   const taskName = inputField.value.trim();
-   let taskListName = "Main Task List";
-   if (!taskName) return;
+	const taskName = inputNameField.value.trim();
+	if (!taskName) return;
 
-   // save task to localStorage
-   saveTaskInStorage(taskListName, taskName);
+	const taskListName = 'Main Task List';
+	const taskIsDone = false;
+	console.log(inputNameField.parentElement.querySelector('input[type="date"]'));
+	const taskDate = createTaskSection.querySelector('input[type="date"]')?.value || 'not set';
+	const taskTime = createTaskSection.querySelector('input[type="time"]')?.value || 'not set';
 
-   // reveal new task in DOM
-   //revealTaskInDOM(taskName, taskId);
-   //e.target.blur();
-
-   inputField.value = "";
+	saveTaskInStorage(taskListName, taskName, taskIsDone, taskDate, taskTime);
+	revealTaskInDOM(taskListName, taskName, taskIsDone, taskDate, taskTime);
+	//e.target.blur();
+	inputNameField.value = '';
 };
 
-function saveTaskInStorage(taskListName = [], name) {
-   let taskObj = {
-      taskName: name,
-      taskIsDone: false,
-   };
-   let tasksInTaskList = JSON.parse(localStorage.getItem(taskListName));
-   tasksInTaskList.push(taskObj);
-   localStorage.setItem(taskListName, JSON.stringify(tasksInTaskList));
+function saveTaskInStorage(taskListName, name) {
+	let taskObj = {
+		taskName: name,
+		taskIsDone: false,
+	};
+	let tasksInTaskList = JSON.parse(localStorage.getItem(taskListName)) || [];
+	tasksInTaskList.push(taskObj);
+	localStorage.setItem(taskListName, JSON.stringify(tasksInTaskList));
 }
 
-function revealTaskInDOM(taskListName, taskName, taskIsDone) {
-   let newTask = document.createElement("li");
-   newTask.classList.add("list__item");
-   if (taskIsDone) newTask.setAttribute("done", "");
-   newTask.innerHTML = `
-      <div class="list__item_name">${taskName}</div>
+function revealTaskInDOM(taskListName, taskName, taskIsDone, taskDate, taskTime) {
+	let newTask = document.createElement('li');
+	newTask.classList.add('list__item');
+	if (taskIsDone) newTask.setAttribute('done', '');
+	newTask.innerHTML = `
+      <div class="list__item_main">
+         <div class="list__item_name">${taskName}</div>
+         <div class="list__item_deadline">
+            <div class="list__item_date">${taskDate}</div>
+            <div class="list__item_time">${taskTime}</div>
+         </div>
+      </div>
       <div class="list__item_buttons">
          <button class="list__item_delete">X</button>
          <button class="list__item_done">Done</button>
       </div>
    `;
-   taskList.appendChild(newTask);
+	taskListInDOM.appendChild(newTask);
 
-   // initialize task buttons
-   let deleteButton = newTask.querySelector(".list__item_delete");
-   let doneButton = newTask.querySelector(".list__item_done");
-   deleteButton.onclick = deleteTask;
-   doneButton.onclick = doneTask;
+	// initialize task buttons
+	let deleteButton = newTask.querySelector('.list__item_delete');
+	let doneButton = newTask.querySelector('.list__item_done');
+	deleteButton.onclick = deleteTask;
+	doneButton.onclick = doneTask;
 }
 
 const deleteTask = (e) => {
-   const task = e.target.closest(".list__item");
-   const taskId = task.getAttribute("data-task-id");
-   task.remove();
-   //delete localStorage[taskId];
+	const task = e.target.closest('.list__item');
+	const taskListName = 'Main Task List';
+	const taskId = Array.from(task.parentElement.children).indexOf(task);
+	task.remove();
+
+	let taskListFromStorage = JSON.parse(localStorage.getItem(taskListName));
+	taskListFromStorage.splice(taskId, 1);
+	localStorage.setItem(taskListName, JSON.stringify(taskListFromStorage));
 };
 
 const doneTask = (e) => {
-   const task = e.target.parentElement.parentElement;
-   const taskId = task.getAttribute("data-task-id");
-   task.toggleAttribute("done");
-   const taskDataFromLS = JSON.parse(localStorage.getItem(taskId));
-   let state = taskDataFromLS.taskDone;
-   taskDataFromLS.taskDone = !state;
-   localStorage.setItem(taskId, JSON.stringify(taskDataFromLS));
-   console.log(taskDataFromLS);
+	const task = e.target.parentElement.parentElement;
+	const taskListName = 'Main Task List';
+	const taskId = Array.from(task.parentElement.children).indexOf(task);
+	task.toggleAttribute('done');
+
+	let taskListFromStorage = JSON.parse(localStorage.getItem(taskListName));
+	let taskObj = taskListFromStorage[taskId];
+	taskObj.taskIsDone = !taskObj.taskIsDone;
+	taskListFromStorage.splice(taskId, 1, taskObj);
+	localStorage.setItem(taskListName, JSON.stringify(taskListFromStorage));
 };
 
-const loadTaskList = () => {
-   if (localStorage.length === 0) return;
+const loadTaskList = (taskListName) => {
+	if (!localStorage[taskListName]) return;
+	let taskList = JSON.parse(localStorage[taskListName]);
 
-   let taskList = Object.assign(localStorage);
-   console.log(taskList);
+	for (let i = 0; i < taskList.length; i++) {
+		let taskData = taskList[i];
 
-   for (let i = 0; i < taskList.length; i++) {
-      // ключ - это id и порядковый номер задачи туду листа
-      let taskId = taskList.key(i);
-      let taskData = JSON.parse(taskList.getItem(taskList.key(i)));
+		let taskName = taskData.taskName;
+		let taskIsDone = taskData.taskIsDone;
 
-      let taskName = taskData.taskName;
-      let taskDone = taskData.taskDone;
-
-      //передаю данные для создания элемента в DOM
-      revealTaskInDOM(taskName, taskId, taskIsDone);
-   }
+		//передаю данные для создания элемента в DOM
+		revealTaskInDOM(taskListName, taskName, taskIsDone);
+	}
 };
 
-loadTaskList();
 addTaskButton.onclick = createNewTask;
-inputField.onkeydown = (e) => {
-   if (e.code === "Enter") createNewTask(e);
-   if (e.key === "Escape") inputField.blur();
+inputNameField.onkeydown = (e) => {
+	if (e.code === 'Enter') createNewTask(e);
+	if (e.key === 'Escape') inputNameField.blur();
 };
 
-let taskListExample = [
-   {
-      taskName: "cold shower",
-      taskIsDone: false,
-   },
-   {
-      taskName: "drink water 2L",
-      taskIsDone: false,
-   },
-];
+setDateBtn.onclick = (e) => {
+	const inputDate = createTaskSection.querySelector(`input[type="date"]`);
+	inputDate.style.display = 'inline-block';
+	setTimeBtn.style.display = 'inline-block';
+	e.target.style.display = 'none';
+};
+
+setTimeBtn.onclick = (e) => {
+	const inputTime = createTaskSection.querySelector(`input[type="time"]`);
+	inputTime.style.display = 'inline-block';
+	e.target.style.display = 'none';
+};
+
+loadTaskList('Main Task List');
+
+/* let taskListExample = [
+	{
+		taskName: 'cold shower',
+		taskIsDone: false,
+	},
+	{
+		taskName: 'drink water 2L',
+		taskIsDone: false,
+	},
+]; */
