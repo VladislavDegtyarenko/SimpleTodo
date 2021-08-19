c09f527cfa50b6f86e24e038aaf3fb4ac5b25617
Vladislav Degtyarenko <vladislavdegtyarenko@gmail.com>
2021-08-20 00:19:45 +0300

    delete unused variables

--- app.js
+++ app.js
@@ -1,69 +1,66 @@
-const createTaskSection = document.querySelector(".newtask"),
-   /* setDateMenu = createTaskSection.querySelector(".setDateMenu"), */
-   addTaskButton = createTaskSection.querySelector(".newtask__create");
-
-const taskListInDOM = document.querySelector(".list__items"),
-   taskListName = "Main Task List",
-   taskListControlBtns = document.querySelector(".list__buttons");
+const createTaskSection = document.querySelector('.newtask'),
+	taskListInDOM = document.querySelector('.list__items'),
+	taskListName = 'Main Task List',
+	taskListControlBtns = document.querySelector('.list__buttons');
 
 const createNewTask = (input) => {
-   const taskName = input.value.trim();
-   if (!taskName) return;
+	const taskName = input.value.trim();
+	if (!taskName) return;
 
-   const taskIsDone = false;
-   const taskPriority = createTaskSection.querySelector("#priority").value;
-   const taskDate = createTaskSection.querySelector(".setDateMenu").getAttribute("data-date"),
-      taskTime = null;
+	const taskIsDone = false;
+	const taskPriority = createTaskSection.querySelector('#priority').value;
+	const taskDate = createTaskSection.querySelector('.setDateMenu').getAttribute('data-date'),
+		taskTime = null;
 
-   const taskData = {
-      taskName,
-      taskIsDone,
-      taskDate,
-      taskTime,
-      taskPriority,
-   };
+	const taskData = {
+		taskName,
+		taskIsDone,
+		taskDate,
+		taskTime,
+		taskPriority,
+	};
 
-   saveTaskInStorage(taskData);
-   revealTaskInDOM(taskData);
-   input.value = "";
-   //input.blur();
+	saveTaskInStorage(taskData);
+	revealTaskInDOM(taskData);
+	input.value = '';
+	//input.blur();
 };
 
 const saveTaskInStorage = (taskData) => {
-   let tasksInTaskList = JSON.parse(localStorage.getItem(taskListName)) || [];
-   tasksInTaskList.push(taskData);
-   localStorage.setItem(taskListName, JSON.stringify(tasksInTaskList));
+	let tasksInTaskList = JSON.parse(localStorage.getItem(taskListName)) || [];
+	tasksInTaskList.push(taskData);
+	localStorage.setItem(taskListName, JSON.stringify(tasksInTaskList));
 };
 
 const deleteTask = (e) => {
-   const thisTask = e.target.closest(".list__item");
-   const taskId = Array.from(thisTask.parentElement.children).indexOf(thisTask);
-   thisTask.remove();
+	const thisTask = e.target.closest('.list__item');
+	const taskId = Array.from(thisTask.parentElement.children).indexOf(thisTask);
+	thisTask.remove();
 
-   let taskListFromStorage = JSON.parse(localStorage.getItem(taskListName));
-   taskListFromStorage.splice(taskId, 1);
-   localStorage.setItem(taskListName, JSON.stringify(taskListFromStorage));
+	let taskListFromStorage = JSON.parse(localStorage.getItem(taskListName));
+	taskListFromStorage.splice(taskId, 1);
+	localStorage.setItem(taskListName, JSON.stringify(taskListFromStorage));
 };
 
 const doneTask = (e) => {
-   const thisTask = e.target.parentElement.parentElement;
-   const taskId = Array.from(thisTask.parentElement.children).indexOf(thisTask);
-   thisTask.toggleAttribute("done");
+	const thisTask = e.target.parentElement.parentElement;
+	const taskId = Array.from(thisTask.parentElement.children).indexOf(thisTask);
+	thisTask.toggleAttribute('done');
 
-   let taskListFromStorage = JSON.parse(localStorage.getItem(taskListName));
-   let taskObj = taskListFromStorage[taskId];
-   taskObj.taskIsDone = !taskObj.taskIsDone;
-   taskListFromStorage.splice(taskId, 1, taskObj);
-   localStorage.setItem(taskListName, JSON.stringify(taskListFromStorage));
+	let taskListFromStorage = JSON.parse(localStorage.getItem(taskListName));
+	let taskObj = taskListFromStorage[taskId];
+	taskObj.taskIsDone = !taskObj.taskIsDone;
+	taskListFromStorage.splice(taskId, 1, taskObj);
+	localStorage.setItem(taskListName, JSON.stringify(taskListFromStorage));
 };
 
 const revealTaskInDOM = (taskData) => {
-   let { taskName, taskIsDone, taskDate, taskTime, taskPriority } = taskData;
+	let { taskName, taskIsDone, taskDate, taskTime, taskPriority } = taskData;
 
-   let taskDiv = document.createElement("li");
-   taskDiv.classList.add("list__item");
-   taskDiv.setAttribute("data-priority", taskPriority);
-   taskDiv.innerHTML = `
+	let taskDiv = document.createElement('li');
+	taskDiv.classList.add('list__item');
+	taskDiv.setAttribute('data-priority', taskPriority);
+	taskDiv.innerHTML = `
 		<label>
 			<input type="checkbox" />
 		</label>
@@ -76,257 +73,263 @@ const revealTaskInDOM = (taskData) => {
          <button class="list__item_delete">X</button>
       </div>
    `;
-   taskListInDOM.appendChild(taskDiv);
+	taskListInDOM.appendChild(taskDiv);
 
-   if (taskIsDone) {
-      taskDiv.setAttribute("done", "");
-      taskDiv.querySelector("label input").checked = true;
-   }
+	if (taskIsDone) {
+		taskDiv.setAttribute('done', '');
+		taskDiv.querySelector('label input').checked = true;
+	}
 
-   if (taskDate) {
-      taskDiv.setAttribute("data-date", taskDate);
-      let listItemMainDiv = taskDiv.querySelector(".list__item_name");
+	if (taskDate) {
+		taskDiv.setAttribute('data-date', taskDate);
+		let listItemMainDiv = taskDiv.querySelector('.list__item_name');
 
-      let dateDiv = document.createElement("div");
-      dateDiv.classList.add("list__item_date");
+		let dateDiv = document.createElement('div');
+		dateDiv.classList.add('list__item_date');
 
-      let relativeDate = fullDateToRelative(taskDate);
-      dateDiv.innerHTML = relativeDate;
+		let relativeDate = fullDateToRelative(taskDate);
+		dateDiv.innerHTML = relativeDate;
 
-      insertAfter(dateDiv, listItemMainDiv);
-   }
+		insertAfter(dateDiv, listItemMainDiv);
+	}
 
-   if (taskTime) {
-      let timeDiv = document.createElement("div");
-      let dateDiv = taskDiv.querySelector(".list__item_date");
-      timeDiv.classList.add("list__item_date");
-      timeDiv.innerHTML = taskTime;
-      insertAfter(timeDiv, dateDiv);
-   }
+	if (taskTime) {
+		let timeDiv = document.createElement('div');
+		let dateDiv = taskDiv.querySelector('.list__item_date');
+		timeDiv.classList.add('list__item_date');
+		timeDiv.innerHTML = taskTime;
+		insertAfter(timeDiv, dateDiv);
+	}
 
-   // initialize task buttons
-   let deleteButton = taskDiv.querySelector(".list__item_delete");
-   let doneButton = taskDiv.querySelector("label input");
-   deleteButton.onclick = deleteTask;
-   doneButton.onclick = doneTask;
+	// initialize task buttons
+	let deleteButton = taskDiv.querySelector('.list__item_delete');
+	let doneButton = taskDiv.querySelector('label input');
+	deleteButton.onclick = deleteTask;
+	doneButton.onclick = doneTask;
 };
 
 const revealAllTasksLoop = (taskList) => {
-   for (let i = 0; i < taskList.length; i++) {
-      let taskData = taskList[i];
-      revealTaskInDOM(taskData);
-   }
+	for (let i = 0; i < taskList.length; i++) {
+		let taskData = taskList[i];
+		revealTaskInDOM(taskData);
+	}
 };
 
 const initTaskList = () => {
-   if (!localStorage[taskListName]) return;
-   let taskList = JSON.parse(localStorage[taskListName]);
-   revealAllTasksLoop(taskList);
+	if (!localStorage[taskListName]) return;
+	let taskList = JSON.parse(localStorage[taskListName]);
+	revealAllTasksLoop(taskList);
 };
 
 const sortTasks = (sortBtn, isReverse) => {
-   if (!localStorage[taskListName]) return;
-   const taskList = JSON.parse(localStorage[taskListName]),
-      sortBtnId = sortBtn.id,
-      sortOrder = isReverse ? -1 : 1,
-      sortingParameter = (function () {
-         if (sortBtnId === "sortByName") return "taskName";
-         if (sortBtnId === "sortByPriority") return "taskPriority";
-         if (sortBtnId === "sortByDeadline") return "taskDate";
-      })();
+	if (!localStorage[taskListName]) return;
+	const taskList = JSON.parse(localStorage[taskListName]),
+		sortBtnId = sortBtn.id,
+		sortOrder = isReverse ? -1 : 1,
+		sortingParameter = (function () {
+			if (sortBtnId === 'sortByName') return 'taskName';
+			if (sortBtnId === 'sortByPriority') return 'taskPriority';
+			if (sortBtnId === 'sortByDeadline') return 'taskDate';
+		})();
 
-   taskList.sort(function (a, b) {
-      a = a[sortingParameter].toLowerCase();
-      b = b[sortingParameter].toLowerCase();
-      if (a > b) return 1 * sortOrder;
-      if (a == b) return 0 * sortOrder;
-      if (a < b) return -1 * sortOrder;
-   });
+	taskList.sort(function (a, b) {
+		a = a[sortingParameter].toLowerCase();
+		b = b[sortingParameter].toLowerCase();
+		if (a > b) return 1 * sortOrder;
+		if (a == b) return 0 * sortOrder;
+		if (a < b) return -1 * sortOrder;
+	});
 
-   sortBtn.toggleAttribute("reverse");
-   localStorage.setItem(taskListName, JSON.stringify(taskList));
+	sortBtn.toggleAttribute('reverse');
+	localStorage.setItem(taskListName, JSON.stringify(taskList));
 
-   taskListInDOM.innerHTML = "";
-   revealAllTasksLoop(taskList);
+	taskListInDOM.innerHTML = '';
+	revealAllTasksLoop(taskList);
 };
 
 const clearList = () => {
-   taskListInDOM.innerHTML = "";
-   localStorage.removeItem(taskListName);
+	taskListInDOM.innerHTML = '';
+	localStorage.removeItem(taskListName);
 };
 
 const showShortDatesInMenu = (dateMenu) => {
-   const dropdownItems = dateMenu.querySelectorAll(".setDateMenu__dropdown_item");
+	const dropdownItems = dateMenu.querySelectorAll('.setDateMenu__dropdown_item');
 
-   dropdownItems.forEach((item) => {
-      let relativeDate = item.querySelector(".relativeDate").textContent,
-         textDate = item.querySelector(".shortDate");
-      let dateConverted = relativeToShortDate(relativeDate);
-      textDate.innerHTML = dateConverted;
-   });
+	dropdownItems.forEach((item) => {
+		let relativeDate = item.querySelector('.relativeDate').textContent,
+			textDate = item.querySelector('.shortDate');
+		let dateConverted = relativeToShortDate(relativeDate);
+		textDate.innerHTML = dateConverted;
+	});
 };
 
 const relativeToFullDate = (relativeDate) => {
-   if (relativeDate === "No Date") return "";
+	if (relativeDate === 'No Date') return '';
 
-   let dateToday = new Date();
+	let dateToday = new Date();
 
-   if (relativeDate === "Today") {
-      let year = dateToday.getFullYear(),
-         month = dateToday.getMonth() + 1,
-         day = dateToday.getDate();
-      return `${year}-${month}-${day}`;
-   }
+	if (relativeDate === 'Today') {
+		let year = dateToday.getFullYear(),
+			month = dateToday.getMonth() + 1,
+			day = dateToday.getDate();
+		return `${year}-${month}-${day}`;
+	}
 
-   if (relativeDate === "Tomorrow") {
-      let year = dateToday.getFullYear(),
-         month = dateToday.getMonth() + 1,
-         day = dateToday.getDate() + 1;
-      return `${year}-${month}-${day}`;
-   }
+	if (relativeDate === 'Tomorrow') {
+		let year = dateToday.getFullYear(),
+			month = dateToday.getMonth() + 1,
+			day = dateToday.getDate() + 1;
+		return `${year}-${month}-${day}`;
+	}
 
-   if (relativeDate === "This Weekend") {
-      let year = dateToday.getFullYear(),
-         month = dateToday.getMonth() + 1,
-         dayOfTheWeek = (function () {
-            let day = dateToday.getDay();
-            if (day == 0) {
-               day = 7;
-            }
-            return day;
-         })(),
-         day = dateToday.getDate() + 6 - dayOfTheWeek;
+	if (relativeDate === 'This Weekend') {
+		let year = dateToday.getFullYear(),
+			month = dateToday.getMonth() + 1,
+			dayOfTheWeek = (function () {
+				let day = dateToday.getDay();
+				if (day == 0) {
+					day = 7;
+				}
+				return day;
+			})(),
+			day = dateToday.getDate() + 6 - dayOfTheWeek;
 
-      return `${year}-${month}-${day}`;
-   }
+		return `${year}-${month}-${day}`;
+	}
 
-   if (relativeDate === "Next Week") {
-      let year = dateToday.getFullYear(),
-         month = dateToday.getMonth() + 1,
-         dayOfTheWeek = (function () {
-            let day = dateToday.getDay();
-            if (day == 0) {
-               day = 7;
-            }
-            return day;
-         })(),
-         day = dateToday.getDate() + 8 - dayOfTheWeek;
+	if (relativeDate === 'Next Week') {
+		let year = dateToday.getFullYear(),
+			month = dateToday.getMonth() + 1,
+			dayOfTheWeek = (function () {
+				let day = dateToday.getDay();
+				if (day == 0) {
+					day = 7;
+				}
+				return day;
+			})(),
+			day = dateToday.getDate() + 8 - dayOfTheWeek;
 
-      return `${year}-${month}-${day}`;
-   }
+		return `${year}-${month}-${day}`;
+	}
 };
 
 const relativeToShortDate = (relativeDate) => {
-   if (relativeDate === "No Date") return null;
+	if (relativeDate === 'No Date') return null;
 
-   let dateToday = new Date();
+	let dateToday = new Date();
 
-   if (relativeDate === "Today") {
-      let weekdayShort = dateToday.toLocaleString("default", { weekday: "short" });
-      return weekdayShort;
-   }
+	if (relativeDate === 'Today') {
+		let weekdayShort = dateToday.toLocaleString('default', { weekday: 'short' });
+		return weekdayShort;
+	}
 
-   if (relativeDate === "Tomorrow") {
-      let nextDay = new Date(dateToday.setDate(dateToday.getDate() + 1));
-      let nextDayShort = nextDay.toLocaleString("default", { weekday: "short" });
-      return nextDayShort;
-   }
+	if (relativeDate === 'Tomorrow') {
+		let nextDay = new Date(dateToday.setDate(dateToday.getDate() + 1));
+		let nextDayShort = nextDay.toLocaleString('default', { weekday: 'short' });
+		return nextDayShort;
+	}
 
-   if (relativeDate === "This Weekend") return "Sat";
+	if (relativeDate === 'This Weekend') return 'Sat';
 
-   if (relativeDate === "Next Week") {
-      let nextMonday = new Date(dateToday.setDate(dateToday.getDate() + ((1 + 7 - dateToday.getDay()) % 7)));
-      let nextMondayShort = nextMonday.toLocaleString("default", { weekday: "short", day: "numeric", month: "short" });
-      return nextMondayShort;
-   }
+	if (relativeDate === 'Next Week') {
+		let nextMonday = new Date(
+			dateToday.setDate(dateToday.getDate() + ((1 + 7 - dateToday.getDay()) % 7))
+		);
+		let nextMondayShort = nextMonday.toLocaleString('default', {
+			weekday: 'short',
+			day: 'numeric',
+			month: 'short',
+		});
+		return nextMondayShort;
+	}
 };
 
 const fullDateToRelative = (fullDate) => {
-   let dateToday = new Date(),
-      someDate = new Date(fullDate);
+	let dateToday = new Date(),
+		someDate = new Date(fullDate);
 
-   if (
-      someDate.getDate() == dateToday.getDate() &&
-      someDate.getMonth() == dateToday.getMonth() &&
-      someDate.getFullYear() == dateToday.getFullYear()
-   ) {
-      return "Today";
-   }
+	if (
+		someDate.getDate() == dateToday.getDate() &&
+		someDate.getMonth() == dateToday.getMonth() &&
+		someDate.getFullYear() == dateToday.getFullYear()
+	) {
+		return 'Today';
+	}
 
-   if (
-      someDate.getDate() == dateToday.getDate() + 1 &&
-      someDate.getMonth() == dateToday.getMonth() &&
-      someDate.getFullYear() == dateToday.getFullYear()
-   ) {
-      return "Tomorrow";
-   }
+	if (
+		someDate.getDate() == dateToday.getDate() + 1 &&
+		someDate.getMonth() == dateToday.getMonth() &&
+		someDate.getFullYear() == dateToday.getFullYear()
+	) {
+		return 'Tomorrow';
+	}
 
-   let daysToWeekend = 6 - dateToday.getDate();
-   let daysToNextWeek = 8 - dateToday.getDate();
+	let daysToWeekend = 6 - dateToday.getDate();
+	let daysToNextWeek = 8 - dateToday.getDate();
 
-   if (daysToWeekend < 6 && someDate.getDay() == 6) {
-      return "This Weekend";
-   }
+	if (daysToWeekend < 6 && someDate.getDay() == 6) {
+		return 'This Weekend';
+	}
 
-   if (daysToWeekend < 8 && someDate.getDay() == 1) {
-      return "Next Week";
-   }
+	if (daysToWeekend < 8 && someDate.getDay() == 1) {
+		return 'Next Week';
+	}
 };
 
 const insertAfter = (newNode, existingNode) => {
-   existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+	existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
 };
 
 createTaskSection.onclick = (e) => {
-   const input = createTaskSection.querySelector(".newtask__name");
-   const priority = createTaskSection.querySelector("#priority");
-   const dateMenu = createTaskSection.querySelector(".setDateMenu");
+	const input = createTaskSection.querySelector('.newtask__name');
+	const priority = createTaskSection.querySelector('#priority');
+	const dateMenu = createTaskSection.querySelector('.setDateMenu');
 
-   const dateMenuClick = e.target.closest(".setDateMenu");
-   const dateMenuItemClick = e.target.matches(".setDateMenu__dropdown_item");
-   const createTaskBtnClick = e.target.matches(".newtask__create");
+	const dateMenuClick = e.target.closest('.setDateMenu');
+	const dateMenuItemClick = e.target.matches('.setDateMenu__dropdown_item');
+	const createTaskBtnClick = e.target.matches('.newtask__create');
 
-   if (dateMenuClick) {
-      dateMenu.toggleAttribute("opened");
-      showShortDatesInMenu(dateMenu);
-   } else {
-      dateMenu.removeAttribute("opened");
-   }
+	if (dateMenuClick) {
+		dateMenu.toggleAttribute('opened');
+		showShortDatesInMenu(dateMenu);
+	} else {
+		dateMenu.removeAttribute('opened');
+	}
 
-   if (dateMenuItemClick) {
-      let clickedMenuItemText = e.target.querySelector(".relativeDate").textContent;
-      let menuPlaceholder = dateMenu.querySelector(".setDateMenu__selected");
-      let fullDateConverted = relativeToFullDate(clickedMenuItemText);
-      menuPlaceholder.textContent = clickedMenuItemText;
-      dateMenu.setAttribute("data-date", fullDateConverted);
-      input.focus();
-   }
+	if (dateMenuItemClick) {
+		let clickedMenuItemText = e.target.querySelector('.relativeDate').textContent;
+		let menuPlaceholder = dateMenu.querySelector('.setDateMenu__selected');
+		let fullDateConverted = relativeToFullDate(clickedMenuItemText);
+		menuPlaceholder.textContent = clickedMenuItemText;
+		dateMenu.setAttribute('data-date', fullDateConverted);
+		input.focus();
+	}
 
-   if (createTaskBtnClick) createNewTask(input);
+	if (createTaskBtnClick) createNewTask(input);
 
-   input.onkeydown = (e) => {
-      const enterKey = e.key === "Enter" || e.code === "NumpadEnter" || e.code === 13;
-      const escapeKey = e.key === "Escape";
+	input.onkeydown = (e) => {
+		const enterKey = e.key === 'Enter' || e.code === 'NumpadEnter' || e.code === 13;
+		const escapeKey = e.key === 'Escape';
 
-      if (enterKey) createNewTask(input);
-      if (escapeKey) input.blur();
-   };
+		if (enterKey) createNewTask(input);
+		if (escapeKey) input.blur();
+	};
 
-   priority.onchange = () => input.focus();
+	priority.onchange = () => input.focus();
 };
 
 taskListControlBtns.onclick = (e) => {
-   const sortBtn = e.target.classList.contains("sortBtn"),
-      clearListBtn = e.target.id === "clearList";
+	const sortBtn = e.target.classList.contains('sortBtn'),
+		clearListBtn = e.target.id === 'clearList';
 
-   if (sortBtn) {
-      let sortBtn = e.target;
-      let isReverse = e.target.hasAttribute("reverse");
-      sortTasks(sortBtn, isReverse);
-   }
-   if (clearListBtn) {
-      clearList();
-   }
+	if (sortBtn) {
+		let sortBtn = e.target;
+		let isReverse = e.target.hasAttribute('reverse');
+		sortTasks(sortBtn, isReverse);
+	}
+	if (clearListBtn) {
+		clearList();
+	}
 };
 
 document.onload = initTaskList();
